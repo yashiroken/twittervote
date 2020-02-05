ca6942e79e5d603be15d30c595e84663cfdade4d
八代健太郎 <yashiro.kentaro@icloud.com>
2020-02-05 23:18:34 +0900

    [update] ヘルパー関数の定義

---
+++ api/respond.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func devodeBody(r *http.Request, v interface{}) error {
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func encodeBody(w http.ResponseWriter, r *http.Request, v interface{}) error {
+	return json.NewEncoder(w).Encode(v)
+}
+
+func respond(w http.ResponseWriter, r *http.Request, status int, data interface{}) {
+	w.WriteHeader(status)
+	if data != nil {
+		encodeBody(w, r, data)
+	}
+}
+
+func renpondErr(w http.ResponseWriter, r *http.Request, status int, args ...interface{}) {
+	respond(w, r, status, map[string]interface{}{
+		"error": map[string]interface{}{
+			"message": fmt.Sprint(args...),
+		},
+	})
+}
+
+func respondHTTPErr(w http.ResponseWriter, r *http.Request, status int) {
+	renpondErr(w, r, status, http.StatusText(status))
+}
